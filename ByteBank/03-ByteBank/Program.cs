988c74577a1ac675bb6854f7bf24bdbd21db84df
Evandro <evandrocurriculos2017@gmail.com>
2021-03-21 16:48:19 -0300

    Learning POO
    Understanding to create class, methods, functions and create the references for objects.

---
+++ ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            //This result is false because the address of memory in the reference of object is different.
+            Console.WriteLine($"Igualdade de tipo de referência: {contaDaGabrielaCosta.titular == contaDaGabriela.titular}");
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            //This result is true because here will compare the values of variable type.
+            Console.WriteLine($"Igualdade de tipo de valor: {idade == idadeMaisUmaVez}");
+
+            contaDaGabriela = contaDaGabrielaCosta;
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+
+            if(contaDaGabriela.saldo >= 100)
+            {
+                contaDaGabriela.saldo -= 100;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
